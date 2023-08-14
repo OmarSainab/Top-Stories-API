7a68e081d0be24265cbb6a2b32ebd77136a6dc0e
sainab omar <sainabomar07@gmail.com>
2023-08-14 16:54:53 +0100

    this commit will create GET /api

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
-const { getTopics } = require("./controllers/ncnewscontrollers");
+const { getTopics, getEndpoints } = require("./controllers/ncnewscontrollers");
 
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
+app.get("/api", getEndpoints);
 
 app.use((error, request, response, next) => {
     console.log(error, 'inside error handler')
     response.status(500).send({message: 'error'})
 })
+
 
 
 
